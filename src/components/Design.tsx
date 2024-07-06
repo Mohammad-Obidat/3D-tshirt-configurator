f2af906a9f749ec0f6f6e7bb916c9097696110ce
mohmdSalah <mo.s.obidat@gmail.com>
2024-07-06 13:26:53 +0300

    init Design button & content styling

--- src/components/Design.tsx
+++ src/components/Design.tsx
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { DesignTabs } from '../config/constants/DesignTabs.constant';
+import { TabsProps } from '../interfaces/Tabs.interface';
 import '../styles/Design.css';
 
 const Design: React.FC = () => {
+  const [tabs, setTabs] = useState<TabsProps>(DesignTabs);
+
+  const setActiveTab = (id: number) => {
+    const updatedTabs = tabs.map((tab) => {
+      if (tab.id === id) {
+        return { ...tab, isActive: true };
+      } else {
+        return { ...tab, isActive: false };
+      }
+    });
+    setTabs(updatedTabs);
+  };
+
   return (
     <div className='design-container'>
-      {DesignTabs.map((tab) => (
-        <div key={tab.id} className='design-item'>
-          <div className={`img-container ${tab.isActive}`}>
+      {tabs.map((tab) => (
+        <div
+          key={tab.id}
+          className={`design-item ${tab.isActive ? 'active' : ''}`}
+          onClick={() => setActiveTab(tab.id)}
+        >
+          <div className='img-container'>
             <img src={tab.imageUrl} alt={tab.title} className='design-img' />
             <span>{tab.title}</span>
           </div>

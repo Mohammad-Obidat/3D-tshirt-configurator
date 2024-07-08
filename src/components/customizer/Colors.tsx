1aa8caff0ef7093a42615e96071a305088d5c5ac
mohmdSalah <mo.s.obidat@gmail.com>
2024-07-08 13:20:35 +0300

    Modify the Color component to display colors with styling

--- src/components/customizer/Colors.tsx
+++ src/components/customizer/Colors.tsx
@@ -1,7 +1,34 @@
 import React from 'react';
+import { colors } from '../../config/constants/Colors';
+import '../../styles/Colors.css';
 
 const Colors: React.FC = () => {
-  return <div>Colors</div>;
+  const handleColorChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ): void => {
+    event.preventDefault();
+
+    const { id } = event.target;
+    console.log(id);
+  };
+
+  return (
+    <div className='color-container'>
+      {colors.map((color) => (
+        <div key={color}>
+          <input
+            type='radio'
+            id={color}
+            name='color'
+            onChange={handleColorChange}
+          />
+          <label htmlFor={color} className={color}>
+            <span className={`${color} color-border`}></span>
+          </label>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Colors;
---
+++ src/config/constants/Colors.ts
@@ -0,0 +1,12 @@
+export const colors: Array<string> = [
+  'red',
+  'orange',
+  'yellow',
+  'olive',
+  'green',
+  'teal',
+  'blue',
+  'violet',
+  'purple',
+  'pink',
+];

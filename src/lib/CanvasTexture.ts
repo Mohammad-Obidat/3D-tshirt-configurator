4314966214a34ab064e0ef45cb5ce723511a848b
mohmdSalah <mo.s.obidat@gmail.com>
2024-07-13 21:25:06 +0300

    Adding keys controls

--- src/lib/CanvasTexture.ts
+++ src/lib/CanvasTexture.ts
@@ -2,8 +2,83 @@ import * as THREE from 'three';
 import TextureManager from './TextureManager';
 
 export default class CanvasTextureManager extends TextureManager {
+  private static CANVAS_SIZE = 512;
+  private scale: THREE.Vector2;
+  private rotation: number;
+  private translation: THREE.Vector2;
+  private currentTexture: THREE.Texture | undefined;
+
   constructor(shirtModel: THREE.Object3D) {
     super(shirtModel);
+    this.scale = new THREE.Vector2(1, 1);
+    this.rotation = 0;
+    this.translation = new THREE.Vector2(0, 0);
+
+    this.addKeyListeners();
+  }
+
+  private addKeyListeners(): void {
+    window.addEventListener('keydown', (event) => this.handleKeyDown(event));
+  }
+
+  private handleKeyDown(event: KeyboardEvent): void {
+    const step = 0.01;
+    const angleStep = Math.PI / 18; // 10 degrees
+
+    switch (event.key) {
+      case 'ArrowUp':
+        this.translation.y += step;
+        break;
+      case 'ArrowDown':
+        this.translation.y -= step;
+        break;
+      case 'ArrowLeft':
+        this.translation.x -= step;
+        break;
+      case 'ArrowRight':
+        this.translation.x += step;
+        break;
+      case 'w':
+        this.scale.y += step;
+        break;
+      case 's':
+        this.scale.y -= step;
+        break;
+      case 'd':
+        this.scale.x += step;
+        break;
+      case 'a':
+        this.scale.x -= step;
+        break;
+      case 'r':
+        this.rotation += angleStep;
+        break;
+      case 'R':
+        this.rotation -= angleStep;
+        break;
+      default:
+        return; // Exit if the key is not handled
+    }
+
+    this.updateUVTransform(this.currentTexture);
+  }
+
+  private updateUVTransform(texture: THREE.Texture | undefined): void {
+    // Apply the transformation to the UV coordinates
+
+    const uvTransform = new THREE.Matrix3();
+
+    uvTransform
+      .identity()
+      .translate(-0.5, -0.5)
+      .scale(this.scale.x, this.scale.y)
+      .rotate(this.rotation)
+      .translate(this.translation.x, this.translation.y)
+      .translate(0.5, 0.5);
+
+    texture!.matrix = uvTransform;
+    texture!.matrixAutoUpdate = false;
+    texture!.needsUpdate = true;
   }
 
   applyTextInput(controlTab: string, textInput: string): void {
@@ -45,22 +120,21 @@ export default class CanvasTextureManager extends TextureManager {
   ): THREE.CanvasTexture {
     const canvas = document.createElement('canvas');
     const context = canvas.getContext('2d');
-    const canvasWidth = 512;
-    const canvasHeight = 512;
 
-    if (context) {
-      canvas.width = canvasWidth;
-      canvas.height = canvasHeight;
-      context.clearRect(0, 0, canvasWidth, canvasHeight);
-      drawContent(context, canvasWidth, canvasHeight);
-
-      const canvasTexture = new THREE.CanvasTexture(canvas);
-      canvasTexture.anisotropy = 16;
-      canvasTexture.needsUpdate = true;
-
-      return canvasTexture;
+    if (!context) {
+      throw new Error('Failed to create canvas context');
     }
-    throw new Error('Failed to create canvas context');
+
+    canvas.width = CanvasTextureManager.CANVAS_SIZE;
+    canvas.height = CanvasTextureManager.CANVAS_SIZE;
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    drawContent(context, canvas.width, canvas.height);
+
+    const canvasTexture = new THREE.CanvasTexture(canvas);
+    canvasTexture.anisotropy = 16;
+    canvasTexture.needsUpdate = true;
+
+    return canvasTexture;
   }
 
   applyCanvasTextureToMesh(
@@ -68,6 +142,15 @@ export default class CanvasTextureManager extends TextureManager {
     canvasTexture: THREE.Texture,
     targetMeshName: string
   ): void {
+    const scaleFactorMapping: { [key: string]: [number, number] } = {
+      Cloth_mesh_24: [2, -1],
+      Cloth_mesh_3: [0.65, -1],
+      Cloth_mesh_9: [1, -0.75],
+      Cloth_mesh_15: [1, -0.75],
+    };
+
+    const scaleFactor = scaleFactorMapping[targetMeshName] || [2, -1];
+
     model.traverse((child) => {
       if (child instanceof THREE.Mesh && child.name === targetMeshName) {
         canvasTexture.wrapT = canvasTexture.wrapS = THREE.ClampToEdgeWrapping;
@@ -75,26 +158,10 @@ export default class CanvasTextureManager extends TextureManager {
         const material = this.createTextureMaterial(canvasTexture);
         const geometry = child.geometry.clone();
 
-        if (targetMeshName === 'Cloth_mesh_24') {
-          const scaleFactorX = 2;
-          const scaleFactorY = -1;
-          this.adjustGeometry(geometry, scaleFactorX, scaleFactorY);
-        } else if (targetMeshName === 'Cloth_mesh_3') {
-          const scaleFactorX = 0.65;
-          const scaleFactorY = -1;
-          this.adjustGeometry(geometry, scaleFactorX, scaleFactorY);
-        } else if (targetMeshName === 'Cloth_mesh_9') {
-          //   const material = new THREE.MeshBasicMaterial({
-          //     map: canvasTexture,
-          //     transparent: true,
-          //     color: new THREE.Color(0xff0000),
-          //   });
-          //   const scaleFactorX = 1;
-          //   const scaleFactorY = -1;
-          //   this.adjustGeometry(geometry, scaleFactorX, scaleFactorY);
-        }
+        this.adjustGeometry(geometry, scaleFactor[0], scaleFactor[1]);
+
         const mesh = new THREE.Mesh(geometry, material);
-        console.log(mesh);
+        this.currentTexture = canvasTexture;
         child.add(mesh);
       }
     });
